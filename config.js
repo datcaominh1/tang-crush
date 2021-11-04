3755ce5ee3393808abbfeff4e004a2691fd289da
datcaominh1 <93699795+datcaominh1@users.noreply.github.com>
2021-11-04 13:18:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    introTitle: 'heo à!',
+    introDesc: `a mún nói là a mún đấm em vl , ăn j mà suốt ngày quạu r dỗi :))`,
+    btnIntro: ':>>',
+    btnIntro: ':<<',
+    title: 'em có stupid ko 😙',
+    desc: 'trả lời ',
+    btnYes: 'Có',
+    btnNo: 'Không :3',
+    question:'BAO LÂU BÁN ĐƯỢC MỘT TỶ GÓI MÈ ??',
+    btnReply: 'GỬI',
+    reply: 'TRẢ LỜI',
+    mess: 'hơ hơ ',
+    messDesc: 'HNAY HỌC TIẾNG ANH VS ANH NHAA',
+    btnAccept: 'Okiiiii lun ',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
